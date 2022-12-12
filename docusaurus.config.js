c65547dd22785299d6054aef702ca5d88709b9c7
franjoespejo <franjoespejo@gmail.com>
2022-12-12 01:32:13 +0100

    test-1

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,26 +10,15 @@ const katex = require('rehype-katex');
 module.exports = {
   themeConfig: {
     metadata: [
-      { name: 'og:image', content: 'https://divalabs.org/metalogo.png' },
-      { name: 'test', content: 'https://divalabs.org/metalogo.png' },
       { name: 'test', content: 'test' },
-      { name: 'twitter:image', content: 'https://divalabs.org/metalogo.png' },
-      {name: 'og:title', content: 'Ethereum liquid staking using distributed validators'},
-      {name: 'og:description', content: 'Diva is a ETH Liquid Staking protocol powered by Distributed Validators that anyone can run with a low number of ETHs.'},
-      {name: 'description', content: 'Diva is a ETH Liquid Staking protocol powered by Distributed Validators that anyone can run with a low number of ETHs.'},
-      {name: 'twitter:card', content: 'summary_large_image'},
-      {name: 'twitter:url', content: 'https://divalabs.org/'},
-      {name: 'og:url', content: 'https://divalabs.org/'},
-      {name: 'twitter:title', content: 'Ethereum liquid staking using distributed validators'},
-      {name: 'twitter:description', content: 'Diva is a ETH Liquid Staking protocol powered by Distributed Validators that anyone can run with a low number of ETHs.'},
-    ],
+ ],
   },
 };
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Diva docs TEST',
-  tagline: 'Diva Labs TEST',
+  title: 'Diva docs',
+  tagline: 'Diva Labs',
   url: 'https://docs.divalabs.org',
   baseUrl: '/',
   onBrokenLinks: 'throw',
